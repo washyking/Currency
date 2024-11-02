0d81706756c380c0f7fc97fa34588711d2540fd6
washyking <maiblefive201@gmail.com>
2024-11-02 19:38:47 +0800

    .

---
+++ currency/src/Components/curr.js
@@ -0,0 +1,13 @@
+import './curr.css'
+
+function curr(){
+    return (
+        <div className=''>
+            <h1>
+                hi
+            </h1>
+        </div>
+    );
+}
+
+export default curr
